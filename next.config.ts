5121e36b880b8ee597f9b4daecb91bcd67f2c4dd
Eleni Kotsiri <elkots@outlook.com>
2025-02-11 15:14:45 +0200

    fixes, next-theme package for theme mode, loading page, not-found page

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // For development only
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; // For development only
 
 export default nextConfig;
