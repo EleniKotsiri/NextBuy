537aaab968a9d85491475f0b034bbc75f57306a6
Eleni Kotsiri <elkots@outlook.com>
2025-03-26 16:43:40 +0200

    packages fixes

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,7 +1,7 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { PrismaNeon } from '@prisma/adapter-neon';
-import { PrismaClient } from '@prisma/client';
-import ws from 'ws';
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
 neonConfig.webSocketConstructor = ws;
@@ -30,3 +30,28 @@ export const prisma = new PrismaClient({ adapter }).$extends({
     },
   },
 });
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+//   globalForPrisma.prisma ??
+//   new PrismaClient({ adapter }).$extends({
+//     result: {
+//       product: {
+//         price: {
+//           compute(product) {
+//             return product.price.toString();
+//           },
+//         },
+//         rating: {
+//           compute(product) {
+//             return product.rating.toString();
+//           },
+//         },
+//       },
+//     },
+//   });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
